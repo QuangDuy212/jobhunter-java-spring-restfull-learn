d123a5f4f00cc79c58133895a892745122f79692
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-08-20 09:17:38 +0700

    video72

--- src/main/java/vn/hoidanit/jobhunter/controller/CompanyController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/CompanyController.java
@@ -4,11 +4,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import vn.hoidanit.jobhunter.domain.Company;
+import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.CompanyService;
+import vn.hoidanit.jobhunter.util.error.IdInvalidException;
+
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -23,6 +32,36 @@ public class CompanyController {
     public ResponseEntity<Company> createNewCompany(@Valid @RequestBody Company reqCompany) {
         Company company = this.companyService.handleCreateCompany(reqCompany);
         return ResponseEntity.status(HttpStatus.CREATED).body(company);
+    }
+
+    @GetMapping("/companies")
+    public ResponseEntity<List<Company>> fetchAllCompanies() {
+        List<Company> companies = this.companyService.fetchAllCompanies();
+        return ResponseEntity.ok(companies);
+    }
+
+    @GetMapping("/companies/{id}")
+    public ResponseEntity<Company> fetchCompanyById(@PathVariable("id") long id) {
+        Optional<Company> company = this.companyService.fetchCompanyById(id);
+        // return ResponseEntity.status(HttpStatus.OK).body(user);
+        return ResponseEntity.ok(company.get());
+    }
+
+    @PutMapping("/companies")
+    public ResponseEntity<Company> updateAUser(@RequestBody Company rqCompany) {
+        Company company = this.companyService.handleUpdateCompany(rqCompany);
+        // return ResponseEntity.status(HttpStatus.OK).body(ericUser);
+        return ResponseEntity.ok(company);
+    }
+
+    @DeleteMapping("/companies/{id}")
+    public ResponseEntity<Void> deleteCompany(@PathVariable("id") long id) throws IdInvalidException {
+        if (id >= 1500) {
+            throw new IdInvalidException("Id khong lon hon 1500");
+        }
+        this.companyService.handleDeleteCompany(id);
+        // return ResponseEntity.status(HttpStatus.OK).body("id: " + id);
+        return ResponseEntity.ok(null);
     }
 
 }
--- src/main/java/vn/hoidanit/jobhunter/service/CompanyService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/CompanyService.java
@@ -1,8 +1,12 @@
 package vn.hoidanit.jobhunter.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import vn.hoidanit.jobhunter.domain.Company;
+import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.repository.CompanyRespository;
 
 @Service
@@ -15,5 +19,33 @@ public class CompanyService {
 
     public Company handleCreateCompany(Company company) {
         return this.companyRespository.save(company);
+    }
+
+    public List<Company> fetchAllCompanies() {
+        return this.companyRespository.findAll();
+    }
+
+    public Optional<Company> fetchCompanyById(long id) {
+        Optional<Company> company = this.companyRespository.findById(id);
+        return company;
+    }
+
+    public Company handleUpdateCompany(Company company) {
+        Optional<Company> optionalCompany = this.fetchCompanyById(company.getId());
+        if (optionalCompany.isPresent()) {
+            Company currentCompany = optionalCompany.get();
+            currentCompany.setName(company.getName());
+            currentCompany.setAddress(company.getAddress());
+            currentCompany.setDescription(company.getDescription());
+            currentCompany.setLogo(company.getLogo());
+            // update
+            currentCompany = this.companyRespository.save(currentCompany);
+            return currentCompany;
+        }
+        return null;
+    }
+
+    public void handleDeleteCompany(long id) {
+        this.companyRespository.deleteById(id);
     }
 }
