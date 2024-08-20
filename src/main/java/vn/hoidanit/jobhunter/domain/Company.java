bc9a238673cda94ab37c21c8bcb6db15826425c5
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-08-20 08:45:58 +0700

    video70

--- src/main/java/vn/hoidanit/jobhunter/domain/Company.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/Company.java
@@ -2,6 +2,8 @@ package vn.hoidanit.jobhunter.domain;
 
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,6 +14,7 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+import vn.hoidanit.jobhunter.util.SecurityUtil;
 
 @Entity
 @Table(name = "companies")
@@ -28,6 +31,7 @@ public class Company {
     private String description;
     private String address;
     private String logo;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss a", timezone = "GMT+7")
     private Instant createdAt;
     private Instant updatedAt;
     private String createdBy;
@@ -35,7 +39,9 @@ public class Company {
 
     @PrePersist
     public void handleBeforeCreate() {
-        this.createdBy = "hoidanit";
+        this.createdBy = SecurityUtil.getCurrentUserLogin().isPresent() == true
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
         this.createdAt = Instant.now();
     }
 }
