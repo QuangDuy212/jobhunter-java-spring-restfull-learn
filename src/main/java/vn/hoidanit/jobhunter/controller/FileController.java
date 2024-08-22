7d1e62c6f51281d33c596ab324ad8a8dc2956cae
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-08-22 22:04:12 +0700

    video102

--- src/main/java/vn/hoidanit/jobhunter/controller/FileController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/FileController.java
@@ -10,6 +10,7 @@ import vn.hoidanit.jobhunter.service.FileService;
 import vn.hoidanit.jobhunter.util.annotation.ApiMessage;
 import vn.hoidanit.jobhunter.util.error.StorageException;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.time.Instant;
@@ -17,8 +18,15 @@ import java.util.List;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -60,6 +68,29 @@ public class FileController {
 
         ResUploadFileDTO res = new ResUploadFileDTO(uploadedFile, Instant.now());
         return ResponseEntity.ok().body(res);
+    }
+
+    @GetMapping("/files")
+    public ResponseEntity<Resource> download(
+            @RequestParam(name = "fileName", required = false) String fileName,
+            @RequestParam(name = "folder", required = false) String folder)
+            throws StorageException, FileNotFoundException, URISyntaxException {
+        if (fileName == null || folder == null)
+            throw new StorageException("Missing required params: (fileName or folder)");
+
+        // check file length
+        long fileLength = this.fileService.getFileLength(fileName, folder);
+        if (fileLength == 0)
+            throw new StorageException("File with name = " + fileName + " not found");
+
+        // download a file
+        InputStreamResource resource = this.fileService.getResource(fileName, folder);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                .contentLength(fileLength)
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
     }
 
 }
--- src/main/java/vn/hoidanit/jobhunter/service/FileService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/FileService.java
@@ -1,6 +1,8 @@
 package vn.hoidanit.jobhunter.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,6 +14,8 @@ import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.io.File;
 import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 @Service
@@ -46,5 +50,25 @@ public class FileService {
                     StandardCopyOption.REPLACE_EXISTING);
         }
         return finalName;
+    }
+
+    public long getFileLength(String fileName, String folder) throws URISyntaxException {
+        URI uri = new URI(baseURI + folder + "/" + fileName);
+        Path path = Paths.get(uri);
+        File tmpDir = new File(path.toString());
+
+        // file not exist , or file is directory => return 0
+        if (!tmpDir.exists() || tmpDir.isDirectory()) {
+            return 0;
+        }
+        return tmpDir.length();
+    }
+
+    public InputStreamResource getResource(String fileName, String folder)
+            throws URISyntaxException, FileNotFoundException {
+        URI uri = new URI(baseURI + folder + "/" + fileName);
+        Path path = Paths.get(uri);
+        File file = new File(path.toString());
+        return new InputStreamResource(new FileInputStream(file));
     }
 }
